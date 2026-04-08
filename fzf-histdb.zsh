62c24b512a96cb20d972520b4bc00c1ab3fb77ca
Tru <tru.zhou@gmail.com>
2026-04-09 01:43:35 +0800

    feat: bind ctrl-v to toggle-raw in fzf widget
    Avoids conflict with ctrl-r which is used for mode cycling.
    Only affects this plugin's fzf instance.

--- fzf-histdb.zsh
+++ fzf-histdb.zsh
@@ -258,7 +258,7 @@ histdb-fzf-widget() {
       --header='${typ}${NL}${switchhints}${NL}―――――――――――――――――――――――――' --delimiter=' '
       -n2.. --with-nth=2..
       --tiebreak=index --expect='esc,ctrl-r,f1,f2,f3,f4'
-      --bind 'ctrl-d:page-down,ctrl-u:page-up'
+      --bind 'ctrl-d:page-down,ctrl-u:page-up,ctrl-v:toggle-raw'
       --print-query
       --preview='source ${FZF_HISTDB_FILE}; histdb-detail ${HISTDB_FILE} {1}' --preview-window=up:50%:wrap
       --no-hscroll
